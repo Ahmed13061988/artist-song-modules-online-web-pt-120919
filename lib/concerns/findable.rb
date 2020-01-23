b7e2f4ef90c4fb2e89c5ab0fddd7cab4faf30781
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-23 00:29:08 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,6 @@
 module Findable 
-  def find_by_name 
-    self.all.detect do |name|
-      if self.name == name 
-      end 
-    end 
+  def self.find_by_name(name)
+    self.detect{|a| a.name == name}
   end 
       
 end 
